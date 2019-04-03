5ec3b0adcfbc7f6e8dcf740c0e93992b598e7254
Ryan Shaw-Harrison <srazash@protonmail.com>
2019-04-03 11:29:17 +0100

    Presentation changes.

--- thingv1.ps1
+++ thingv1.ps1
@@ -1,12 +1,14 @@
 ﻿
 function Start-MainMenu {
     $apptitle = "
-    w e l c o m e   t o   .   .   .
-    ▀▀█▀▀ ▒█░▒█ ▀█▀ ▒█▄░▒█ ▒█▀▀█  ▀█░█▀ ░ ▄█░ 
-    ░▒█░░ ▒█▀▀█ ▒█░ ▒█▒█▒█ ▒█░▄▄  ░█▄█░ ▄ ░█░ 
-    ░▒█░░ ▒█░▒█ ▄█▄ ▒█░░▀█ ▒█▄▄█  ░░▀░░ █ ▄█▄ 
-    t h e   u l t i m a t e   s c r i p t
-    c r e a t e d   b y   R S H (C) 2 0 1 9
+    +-------------------------------------------+
+    ¦ > w e l c o m e   t o   .   .   .         ¦
+    ¦ ▀▀█▀▀ ▒█░▒█ ▀█▀ ▒█▄░▒█ ▒█▀▀█  ▀█░█▀ ░ ▄█░ ¦
+    ¦ ░▒█░░ ▒█▀▀█ ▒█░ ▒█▒█▒█ ▒█░▄▄  ░█▄█░ ▄ ░█░ ¦
+    ¦ ░▒█░░ ▒█░▒█ ▄█▄ ▒█░░▀█ ▒█▄▄█  ░░▀░░ █ ▄█▄ ¦
+    ¦ > t h e   u l t i m a t e   s c r i p t   ¦
+    ¦ > c r e a t e d   b y   R S H (C) 2 0 1 9 ¦
+    +-------------------------------------------+
     "
     
     # clear the host
@@ -18,10 +20,12 @@ function Start-MainMenu {
     # display the warnings
     Write-Host "!! you are running this in an elevated session, right? you're gonna want to do that !!"
     Write-Host "!! ALSO, if you don't know what you're doing or what you're looking at do us both a favour !!"
-    Write-Host "!! Wnter <Q> to quit or close this window !!`n"
+    Write-Host "!! Enter <Q> to quit or click the windows [ X ] button close this window !!`n"
 
     # display the main menu options
     Write-Host "<1> Cleanup Temp & Junk Files"
+    Write-Host "    <R> Empty all Recycle Bins (all users)"
+    Write-Host "    <U> Clean Windows Update Download Cache"
     Write-Host "<2> . . ."
     Write-Host "<3> . . ."
     Write-Host "<4> . . ."
@@ -34,12 +38,13 @@ function Start-MainMenu {
     if ($menuselect -eq "Q") {
         Clear-Host
         Write-Host "Quitting!"
-        [void](Read-Host "Press Enter to continue")
+        [void](Read-Host "Press [RETURN] to continue...")
+        Clear-Host
         Exit
     } else {
         Clear-Host
         Write-Host "That wasn't a valid choice, please try again!"
-        [void](Read-Host "Press Enter to continue")
+        [void](Read-Host "Press [RETURN] to continue...")
         Start-MainMenu
     }
 
